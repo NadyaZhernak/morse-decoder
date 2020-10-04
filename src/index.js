e581001d0786bf2f0078214a12858bc368195915
Nadya Zhernak <makaronchik28@gmail.com>
2020-10-04 21:06:38 +0300

    feat: add morse-decoder solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let letter = '';
+    let symbol = '';
+    let word = '';
+    for (let i = 0; i < expr.length; i++) {
+        if ((i + 1) % 10 === 0) {
+            letter += expr[i];
+            arr.push(letter);
+            letter = '';
+        } else {
+            letter += expr[i];
+        }        
+    } 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === '*') {
+                letter = ' ';
+            } else if (arr[i][j] === '0') {
+                continue;
+            } else {
+                symbol = arr[i][j] + arr[i][j + 1];
+                if (symbol === '10') {
+                    letter += '.';
+                } else {
+                    letter += '-';
+                }
+                symbol = '';
+                j++;
+            }            
+        }
+        arr[i] = letter;
+        letter = '';
+    } 
+    for (let i = 0; i < arr.length; i++) {
+        word += (arr[i] === ' ') ? ' ' : MORSE_TABLE[arr[i]]; 
+    }
+    return word;
 }
 
 module.exports = {
